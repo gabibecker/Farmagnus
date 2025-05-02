d05183d8f38dccb838ae82cf6e40650f75caaf55
gabi.becker <g.becker@unochapeco.edu.br>
2025-05-02 10:08:10 -0300

    criação da tela de login

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4");
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10");
+        classpath("com.android.tools.build:gradle:8.6.0")
+
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@ dependencyResolutionManagement {
     }
 }
 
+
 rootProject.name = "Farmagnus"
 include(":app")
  

1df9a52e81affac530247f98a7e9c0dccf81b0d4
gabi.becker <g.becker@unochapeco.edu.br>
2025-05-26 23:49:44 -0300

    Criação da tela do medicamento detalhado

---
+++ app/src/main/java/com/example/farmagnus/PedidoDetalhado.kt
@@ -0,0 +1,20 @@
+package com.example.farmagnus
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PedidoDetalhado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pedido_detalhado)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
